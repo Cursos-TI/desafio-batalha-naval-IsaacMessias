f5d327c6f776a00dc280efd422972c494a89fcbb
IsaacMessias <isaacmmsilva80@gmail.com>
2025-08-26 23:09:41 +0000

    intermediario

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,48 +1,69 @@
 #include <stdio.h>
 
+#define linhas 10
+#define colunas 10
 
 
 int main() {
     
     char coluna [10] = {'A','B','C','D','E','F','G','H','I','J'};
     int linha [10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int tabuleiro[10][10] = {
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0}
-    };
-
-    tabuleiro[3][3] = 3; //declarando o valor para cada posição
-    tabuleiro[4][3] = 3; //mantendo o navio na coluna "D", indice "3"
-    tabuleiro[5][3] = 3;
-
-    tabuleiro[1][1] = 3; //declarando o valor para cada posição
-    tabuleiro[1][2] = 3; //manteidno da linha "2", indice "1"
-    tabuleiro[1][3] = 3;
-
+    int tabuleiro[linhas][colunas];
+    
+    printf("TABULEIRO BATALHA NAVAL\n");
     printf("------------------------------------------\n");
     printf("    "); 
-        for(int c = 0; c <= 9; c++){
-            printf(" %c ", coluna[c]);//imprime as letras acima da matriz
-        }
-        printf("\n");  
-        for (int i = 0;  i <= 9 ; i++)
-        {   printf(" %2d ", linha[i]);//imprime o numero ao lado de cada linha 
+    
+    for(int c = 0; c <= 9; c++){
+            printf(" %c ", coluna[c]);}
+            printf("\n");
+            //imprime as letras acima da matriz
+            
+        for(int l = 0; l < linhas; l++){
+            printf(" %2d ", linha[l]);//imprime o numero ao lado de cada linha 
+            
+            for(int c = 0; c < colunas; c++){ 
+                if((l == 0) && ((c > 1) &&(c <= 4)))
+                {
+                    tabuleiro[l][c] = 3; 
+                    printf(" %d ", tabuleiro[l][c]);
 
-            for(int j = 0; j <= 9 ; j++){ //"i" são as linha e "j" são as colunas
-                printf(" %d ", tabuleiro[i][j]); //aqui, imprime o valor de cada indice, linha a linha,
-        }
-        printf("\n");  
+                }
+                
+                else if(((l > 4) && (l <=7) && (c == 9)))
+                {
+                    tabuleiro[l][c] = 3; 
+                    printf(" %d ", tabuleiro[l][c]);
 
+                }
+                
+                else if((l == c) && ((l > 0)&&(l < 4)))
+                {
+                    tabuleiro[l][c] = 3; 
+                    printf(" %d ", tabuleiro[l][c]); 
+
+                }
+                
+                else if((l + c == 9) && ((l > 3) && (l <= 6))) 
+                {
+                    tabuleiro[l][c] = 3; 
+                    printf(" %d ", tabuleiro[l][c]);                        
+                }
+                
+                else if((l + c == 12) && ((l > 6) && (l <= 9))){
+                    tabuleiro[l][c] = 3; 
+                    printf(" %d ", tabuleiro[l][c]);   
+                    
+                }
+                
+                else{
+                    tabuleiro[l][c] = 0;
+                    printf(" %d ", tabuleiro[l][c]);
+                }
+            }
+            printf("\n");
         }
-        printf("\n");
+
 
     return 0;
 }
